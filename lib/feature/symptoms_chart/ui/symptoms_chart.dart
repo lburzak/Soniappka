e2142db960862024d22902a8c877f6a91a8dd6a2
lburzak <kontakt@lukaszburzak.pl>
2023-10-30 22:19:03 +0100

    [symptoms-chart] bump syncfusion libs, improve styling, add localizations

--- lib/feature/symptoms_chart/ui/symptoms_chart.dart
+++ lib/feature/symptoms_chart/ui/symptoms_chart.dart
@@ -1,5 +1,6 @@
 import 'package:easy_beck/feature/symptoms_chart/service/symptoms_chart_state.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class SymptomsChart extends StatelessWidget {
@@ -19,16 +20,35 @@ class SymptomsChart extends StatelessWidget {
           final data = snapshot.requireData;
 
           return SfCartesianChart(
-            primaryXAxis: DateTimeAxis(),
-            primaryYAxis: CategoryAxis(
+            primaryXAxis: DateTimeAxis(
+              axisLine: const AxisLine(color: Colors.black),
+              majorGridLines: const MajorGridLines(width: 0),
+              majorTickLines: const MajorTickLines(color: Colors.black),
+              labelStyle: const TextStyle(color: Colors.black),
+              minorGridLines: const MinorGridLines(color: Colors.black),
               interval: 1,
+              labelAlignment: LabelAlignment.start,
+              intervalType: DateTimeIntervalType.days,
+              axisLabelFormatter: (AxisLabelRenderDetails
+                      axisLabelRenderArgs) =>
+                  ChartAxisLabel(
+                      DateFormat(axisLabelRenderArgs.currentDateFormat, "pl").format(DateTime.fromMillisecondsSinceEpoch(axisLabelRenderArgs.value.toInt())),
+                      const TextStyle()),
+            ),
+            primaryYAxis: NumericAxis(
+              axisLine: const AxisLine(color: Colors.black),
+              majorTickLines: const MajorTickLines(color: Colors.black),
+              labelStyle: const TextStyle(color: Colors.black),
+              majorGridLines: const MajorGridLines(color: Colors.black12),
             ),
             zoomPanBehavior: ZoomPanBehavior(
                 enablePinching: true,
                 enablePanning: true,
                 zoomMode: ZoomMode.x),
             legend: const Legend(isVisible: true),
-            margin: EdgeInsets.zero,
+            margin: const EdgeInsets.all(24),
+            plotAreaBorderWidth: 0,
+            plotAreaBorderColor: Colors.black,
             series: <CartesianSeries>[
               StackedColumnSeries(
                   dataSource: data.symptomsChartDataPoints,
@@ -49,7 +69,7 @@ class SymptomsChart extends StatelessWidget {
                   dataSource: data.testResults,
                   color: Colors.lightGreen,
                   width: 3,
-
+                  legendItemText: "BDI",
                   xValueMapper: (value, _) => value.submissionDateTime,
                   yValueMapper: (value, _) => value.points / 5)
             ],
