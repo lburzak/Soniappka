243a9068b9729987e8e7da917170fda1da39d397
lburzak <kontakt@lukaszburzak.pl>
2023-11-04 18:55:57 +0100

    [splash] implement preservance

--- lib/common/ui/loader_builder.dart
+++ lib/common/ui/loader_builder.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 
 class LoaderBuilder extends StatelessWidget {
   final Loader loader;
+  final void Function()? onReady;
 
-  const LoaderBuilder({super.key, required this.builder, required this.loader});
+  const LoaderBuilder(
+      {super.key,
+      required this.builder,
+      required this.loader,
+      required this.onReady});
 
   final Widget Function(BuildContext context) builder;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: loader.load(),
+        future: loader.load().then((_) => onReady?.call()),
         builder: (context, snapshot) =>
             snapshot.connectionState == ConnectionState.done
                 ? builder(context)
--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,18 @@ import 'package:easy_beck/common/ui/loader_builder.dart';
 import 'package:easy_beck/isar/isar_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 
 void main() async {
-
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   Intl.defaultLocale = "pl_PL";
   final isarContainer = IsarContainer();
   final routerContainer = RouterContainer(
-      (context) => BeckTestQuestionnaireContainer(context),
+          (context) => BeckTestQuestionnaireContainer(context),
       DashboardContainer(isarContainer),
       JournalPageContainer(
           symptomsChartContainer: SymptomsChartContainer(),
@@ -37,30 +39,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final font = GoogleFonts.itimTextTheme();
     return LoaderBuilder(
+        onReady: () { FlutterNativeSplash.remove(); },
         loader: loader,
-        builder: (context) => MaterialApp.router(
-            title: 'Flutter Demo',
-            routerConfig: routerConfig,
-            supportedLocales: const [Locale("pl")],
-            locale: const Locale("pl"),
-            localizationsDelegates: const [
-              AppLocalizations.delegate, // Add this line
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(
-                    seedColor: Color(0xffA0C49D),
-                    background: const Color(0xffC4D7B2)),
-                cardTheme: const CardTheme(surfaceTintColor: Color(0xffF4F2DE), color: Color(0xffF7FFE5)),
-                // iconTheme: const IconThemeData(color: Color(0xffA0C49D)),
-                useMaterial3: true,
-                textTheme: font.copyWith(
-                    labelLarge: font.labelLarge?.copyWith(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                )))));
+        builder: (context) =>
+            MaterialApp.router(
+                title: 'Flutter Demo',
+                routerConfig: routerConfig,
+                supportedLocales: const [Locale("pl")],
+                locale: const Locale("pl"),
+                localizationsDelegates: const [
+                  AppLocalizations.delegate, // Add this line
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                    colorScheme: ColorScheme.fromSeed(
+                        seedColor: Color(0xffA0C49D),
+                        background: const Color(0xffC4D7B2)),
+                    cardTheme: const CardTheme(
+                        surfaceTintColor: Color(0xffF4F2DE),
+                        color: Color(0xffF7FFE5)),
+                    // iconTheme: const IconThemeData(color: Color(0xffA0C49D)),
+                    useMaterial3: true,
+                    textTheme: font.copyWith(
+                        labelLarge: font.labelLarge?.copyWith(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        )))));
   }
 }
